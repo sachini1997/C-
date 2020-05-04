167fca8decd8f9933e702017feb25e02a65bdf6b
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2020-05-04 23:08:51 +0530

    Add files via upload

---
+++ multi_table.cs
@@ -0,0 +1,21 @@
+using System;  
+public class Exercise6  
+{  
+    public static void Main() 
+{
+   int j,n;
+   
+	Console.Write("\n\n");
+    Console.Write("Display the multiplication table:\n");
+    Console.Write("-----------------------------------");
+    Console.Write("\n\n");   
+
+   Console.Write("Input the number (Table to be calculated) : ");
+   n= Convert.ToInt32(Console.ReadLine());   
+   Console.Write("\n");
+   for(j=1;j<=10;j++)
+   {
+     Console.Write("{0} X {1} = {2} \n",n,j,n*j);
+   }
+  }
+}
