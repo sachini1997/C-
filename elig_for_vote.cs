20efd805ed06d08e1e38036c9e7bb5016c8d5331
Sachini Senanayake <48913793+sachini1997@users.noreply.github.com>
2020-05-06 23:32:22 +0530

    Add files via upload

---
+++ elig_for_vote.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Man{
+
+    public static void Main{
+
+int age;
+
+Console.Write("Enter the age\n");
+
+age = Convert .ToInt32 ( Console . ReadLine());
+
+if(age>=18){
+
+Console.Write("Eligible to vote");
+
+}
+
+else{
+
+Console.Write("Not Eligible to vote");
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
+
